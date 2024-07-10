683b8245c73efbd10eef789616a9c6d86bddc183
HereIsSee <biramantvydas@gmail.com>
2024-07-10 22:16:44 +0300

    Add mouse-hover painting, canvas size changes
    Added the Etch-a-Sketch part of the project
    and make it possible to change the size of the
    board

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const container = document.querySelector("#Container");
+const changeSizeButton = document.querySelector("#changeSizeButton");
+
 GenerateGrid(10,10);
+
+changeSizeButton.addEventListener("click", () => {
+    const widthInput = document.querySelector("#width");
+    const heightInput = document.querySelector("#height");
+    const width = parseInt(widthInput.value);
+    const height = parseInt(heightInput.value);
+    
+    while(container.firstChild) { 
+        container.removeChild(container.firstChild); 
+    } 
+    //prompt("HELLOS");
+    GenerateGrid(width, height);
+})
+
 //prompt("DONE");
 
 
 function GenerateGrid(x, y){
 
+    
     for(i =0; i< x;i++){
         const line = document.createElement("div");
         line.classList.add("line");
         container.appendChild(line);
         for(j=0;j<y;j++){
             const item = document.createElement("div");
-            // item.addEventListener("hover", () =>{changeColor(name)})
+            item.addEventListener("mouseover", ChangeColorOfBox);
             item.classList.add("box");
             line.appendChild(item);
         }
     }
+}
+
+function ChangeColorOfBox(event){
+    event.target.style.backgroundColor = "yellow";
 }
